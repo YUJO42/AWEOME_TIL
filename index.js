cb14aeb68694abe356a55fcb71c361084354750b
yujo <yujo@student.42seoul.kr>
2020-06-24 17:01:34 +0900

    basic setting

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(helmet());
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.listen(4000);
