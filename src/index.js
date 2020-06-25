dd162b6b990bcd23d9272907324c7331182a4021
yujo <yujo@student.42seoul.kr>
2020-06-25 13:05:08 +0900

    Router, Controller, routes Setting

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
+const PORT = 4000;
 const app = express();
+const handleListening = () => {
+  console.log(`Listening on : http://localhost:${PORT}`);
+};
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -13,4 +19,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.listen(4000);
+app.use(routes.home, globalRouter);
+
+app.listen(PORT, handleListening);
